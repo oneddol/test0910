317e79572836756343b146631abbe0bd655d65dd
oneddol <68422045+oneddol@users.noreply.github.com>
2020-09-10 15:20:08 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,19 @@
+(function($){
+
+    
+    $('.depth1 > li').hover(
+        function(){
+            $(this).find('.depth2')
+            .stop().slideDown(700)
+        },
+        function(){
+            $(this).find('.depth2')
+            .stop().slideUp(500)
+        }
+    )
+
+    $('#kimBox').load('main.html')
+
+
+
+})(jQuery)
